efc57e3b2f4ea08ddd939b20218d154a183065aa
silayagiz <1035010@dogus.edu.tr>
2014-05-03 15:23:18 +0300

    deneme yapıldı

--- src/com/example/bahcem/AnaEkran.java
+++ src/com/example/bahcem/AnaEkran.java
@@ -19,7 +19,7 @@ public class AnaEkran extends Activity {
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.ana_ekran);
-	        
+	     //github deneme   
 	        String MEYVESEBZE = "meyve_sebze";
 	        String SUSBITKILERI ="sus_bitkileri";
 	     
